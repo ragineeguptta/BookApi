e53d34a76cfde951327e684947f7c1ef59860a88
Raginee Gupta <ragineegupta123@gmail.com>
2026-02-18 23:22:55 +0530

    1 half

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using BookApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookApi.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private Book[] _books = new Book[]
+        {
+            new Book{Id = 1, Author = "Raginee", Title = "Book 1"},
+            new Book{Id = 2, Author = "Gunjan", Title = "Book 2" },
+            new Book{Id = 3, Author = "Neha", Title = "Book 3"}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(_books);
+        }
+
+    }
+}
